d4bfa945bb5974bf3d7cad2521042e631c750bfc
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-06-02 12:10:41 +0600

    colsole log print out to node.js

---
+++ 001 First Code of JS/main.js
@@ -0,0 +1,7 @@
+console.log('Hello World');
+console.log('I love Bangladesh');
+console.log(8801752128520);
+console.log(`My Fav Number is ${9}`);
+console.log('My Fav Number is ' + 9);
+
+console.log(typeof(true));
