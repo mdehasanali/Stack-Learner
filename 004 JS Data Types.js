7cc338aaffa4a5ba78766527cb185c4db96bd5d3
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-06-03 10:22:37 +0600

    js data type  and type conversion all

--- 004 JS Data Types.js
+++ 004 JS Data Types.js
@@ -1,15 +1,61 @@
 // primitive data type
 
-Number,
-String,
-Boolean,
-true, false,
-undefined,
-null
+// Number,
+// String,
+// Boolean,
+// undefined,
+// null
 
 
 // Object data type
 
-Array,
-Object,
-Function
+// Array,
+// Object,
+// Function
+
+
+
+// data type check korte hoi typeof mathor diya 
+
+// ****** Number data type *******
+var mm = Number(10);
+console.log(mm);
+var mms = Number("10")
+console.log(mms);
+var numbers = 10;
+console.log(typeof(numbers)); // number data type
+var rr = 1 / 0;
+console.log(rr);
+
+
+
+// ****** String data type *******
+
+var s1 = "this is a text";
+var s2 = String("this is a text 2");
+var s3 = String(150);
+var s4 = String(105.00);
+console.log(s1, s2, s3, s4);
+console.log(typeof(s1)); // string data type
+
+
+// ****** Boolean data type *******
+
+var boolean1 = true;
+var boolean2 = false;
+var boolean3 = Boolean(true);
+var boolean4 = Boolean(false);
+
+console.log(typeof(boolean1, boolean2, boolean3, boolean4)); // boolean data type
+
+
+
+
+// ****** undefined data type *******
+var undefined;
+console.log(typeof(undefined));
+
+
+// ****** null data type *******
+var nall = null;
+console.log(nall);
