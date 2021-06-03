9bd79b835e4e91099e7d27cb441a72a1afd27f48
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-06-03 10:49:16 +0600

    005 Octal and Hexadecimal Numbers

---
+++ 005 Octal and Hexadecimal Numbers.js
@@ -0,0 +1,17 @@
+// hexadecimal Number 
+
+var hex = 0xa; //10
+var hex2 = 0xff; // 225
+var hex3 = 0xaf; // 175
+var hex4 = 0xb; // 11
+// console.log(hex4);
+
+
+
+
+// octal Number 
+
+var octal = 010; //8
+var octal2 = 0150; // 104
+var octal3 = 0774; // 508
+console.log(octal3);
