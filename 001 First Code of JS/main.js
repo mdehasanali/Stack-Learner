1dedb6fe826c3e7f3d9949e6e7e7235c55c187b0
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-06-03 08:31:29 +0600

    delete folder and html or css file console.log print out to node.js

--- 001 First Code of JS/main.js
+++
@@ -1,7 +0,0 @@
-console.log('Hello World');
-console.log('I love Bangladesh');
-console.log(8801752128520);
-console.log(`My Fav Number is ${9}`);
-console.log('My Fav Number is ' + 9);
-
-console.log(typeof(true));
