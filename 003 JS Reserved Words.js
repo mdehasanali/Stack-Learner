326cdf94ea3d008b92b3ce8c8b058603b8db9ffb
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-06-03 09:09:24 +0600

    // variable name  diklaretion

---
+++ 003 JS Reserved Words.js
@@ -0,0 +1,9 @@
+// variable name  diklaretion
+
+var fullName = "Md. Ehasan Ali";
+var myAccountDetielsNumber = 101;
+var my_account_detiels_number = 102;
+
+console.log(fullName);
+console.log(myAccountDetielsNumber);
+console.log(my_account_detiels_number);
