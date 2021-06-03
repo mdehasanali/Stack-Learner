8e8884036010955c2d5b4bf90ea479b220daff3b
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-06-03 08:44:36 +0600

    variable added

---
+++ 002 What is Variables.js
@@ -0,0 +1,7 @@
+var fName = "Md. Ehasan Ali";
+var age = 18;
+var cgpa = 2.81;
+
+console.log(fName + " is a programer");
+console.log("I am " + age + " years old");
+console.log("My result in " + cgpa);
