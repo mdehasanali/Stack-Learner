c2599061d8e8d3936cbc3037d75031cdfee6052f
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-06-04 10:19:20 +0600

    Arithmetic Assignment, Comparison, Logical Operators alll

--- 006 Operators.js
+++ 006 Operators.js
@@ -1,1 +1,91 @@
-// Arithmetic Operators
+//************* Arithmetic Operators ***************
+// + - * / % ++ --
+
+/*
+var a = 20;
+var b = 10;
+console.log(a + b); // 30 যোগ করবে
+console.log(a - b); // ১০ বিয়োগ করবে
+console.log(a * b); // ২০০ গুণ করবে
+console.log(a / b); // ২ ভাগ করবে
+a = 10
+b = 6
+console.log(a % b); // 4 ভাগশেষ করবে
+*/
+
+/*
+var a = 20;
+var b = 10;
+আগে ১ যোগ করবে
+console.log(++a); // pre Increment
+
+result er por value chenge hobe
+console.log(a++); // post Increment
+
+আগে ১ বিয়োগ করবে
+console.log(--b);// pre Decrement
+
+result er por value chenge hobe
+console.log(b--);// post Decrement
+*/
+
+
+
+
+// *************** Assignment Operators ***************
+
+/*
+var a = 20;
+var b = 10;
+console.log(a += b); // a = (a + b)
+console.log(a -= b); // a = (a - b)
+console.log(a *= b); // a = (a * b)
+console.log(a /= b); // a = (a / b)
+
+a = 10
+b = 6
+console.log(a %= b); // a = (a % b)
+console.log(a **= b); // a = (a ** b)
+*/
+
+
+
+
+
+// ************* Comparison Operators ***************
+
+/**
+ *  ==  	equal to
+ *  ===     equal value and equal type
+ *  !=  	not equal
+ *  !==     not equal value or not equal type
+ *  >       greater than
+ *  <      	less than
+ *  >= 	    greater than or equal to
+ *  <= 	    less than or equal to
+ *  ?       ternary operator
+ */
+
+// var a = 20;
+// var b = 10;
+
+// console.log(a == b); // false
+// console.log(a != b); // true
+// console.log(a > b); // true
+// console.log(a < b); // false
+// console.log(a <= b); // false
+// console.log(a >= b); // true
+
+// var a = "20";
+// var b = 20;
+// console.log(a == b); // true
+// console.log(a === b); // false
+// console.log(a !== b); // ture
+
+
+
+
+// ************* Logical Operators ***************
+// &&	logical and
+// ||	logical or
+// !	logical not
