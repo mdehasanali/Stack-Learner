af5419f69920edde605f0a3ec04331f598f850c8
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-06-05 10:29:06 +0600

    007 statements add

---
+++ 007 JS Statements.js
@@ -0,0 +1,15 @@
+var a = 10
+var b = 20
+var c = 30
+
+if (a === b || a <= c) {
+    console.log('I thik');
+} else {
+    console.log('I am not thik');
+}
+
+if (a === b && a <= c) {
+    console.log('I thik');
+} else {
+    console.log('I am not thik');
+}
