293c1e8a5c1a4221ac70de8ee1fda57ef79df3ec
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-06-06 09:42:44 +0600

    date function add and new date create

--- 009 JS Date Functions .js
+++ 009 JS Date Functions .js
@@ -1,1 +1,14 @@
-var date = new Date("10")
+var date = new Date();
+console.log(date);
+console.log(date.toDateString()); // date string
+console.log(date.toLocaleString()); // somoy and time
+console.log(date.toTimeString()); // time show korbe
+
+console.log(date.getDate())
+console.log(date.getDay())
+console.log(date.getMonth())
+console.log(date.getFullYear())
+console.log(date.getHours())
+console.log(date.getMinutes())
+console.log(date.getSeconds())
+console.log(date.getMilliseconds())
