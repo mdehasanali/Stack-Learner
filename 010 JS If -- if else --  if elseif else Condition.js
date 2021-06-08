2390c8153cd9f20656b2e8b125c13cad8d7a2841
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-06-08 10:09:44 +0600

    if condition add

---
+++ 010 JS If -- if else --  if elseif else Condition.js
@@ -0,0 +1,31 @@
+/*************** If condition *************** */
+var a = 20;
+var b = 100;
+if (a > b) {
+    console.log("a boro b er theke");
+}
+if (a < b) {
+    console.log("a choto b er theke");
+}
+
+
+// even and odd
+var number = 401;
+if (number % 2 === 0) {
+    console.log('This is a Even number');
+}
+if (number % 2 === 1) {
+    console.log('This is a odd number');
+}
+
+// number % 2 === 0 ? console.log("even") : console.log("odd");
+
+var newPassword = "1234587458Ehasan!@#$%^&*()";
+var confromPassword = "1234587458Ehasan!@#$%^&*()";
+
+if (newPassword === confromPassword) {
+    console.log("Passowrd Match");
+}
+if (newPassword !== confromPassword) {
+    console.log("Password is not match");
+}
