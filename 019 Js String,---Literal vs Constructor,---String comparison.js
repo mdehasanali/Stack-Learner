137db95cea2632824f73ed89fa14accbf32d2b46
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-06-13 09:04:12 +0600

    019 Js String,--- Literal vs Constructor,--- JS Escape Notation,---

--- 019 Js String,---Literal vs Constructor,---String comparison.js
+++ 019 Js String,---Literal vs Constructor,---String comparison.js
@@ -3,3 +3,31 @@ var number2 = '10' // string literal
 
 console.log(number1);
 console.log(number2);
+
+
+
+//////////////////////////////////////////////////
+////////////// Literal Constructor ///////////////
+//////////////////////////////////////////////////
+
+var fulName = 'Md. Ehasan Ali' // String literal 
+var fulName2 = String('Md. Ehasan Ali') // String Constructor\
+var fullName3 = fulName.toString()
+
+console.log(fulName);
+console.log(fulName2);
+
+var hobby = 'My fav game\'s is Cricket, Footboll, Carram, Programing '
+console.log(hobby);
+
+var hobby = 'This is a \'String\''
+console.log(hobby);
+
+
+// new line create
+var hobby = 'This is a \n String'
+console.log(hobby);
+
+// backcelas print
+var hobby = 'This is a \\ String'
+console.log(hobby);
