a6ea5f3804b9389005e59a115d13db9a2a60151e
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-06-14 11:35:23 +0600

    array traversing addad

---
+++ array/21 array Traversing.js
@@ -0,0 +1,64 @@
+var number = [5, 2, 4, 7, 3, 6, 9, 4, 2, 5, 4, 1, 2, 5, 3, 9, 9, 8]
+
+// console.log(number);
+// console.log(number.length);
+
+// for (let index = 0; index < number.length; index++) {
+//     console.log(number[index]);
+// }
+
+
+// Array sathe 2 jog kore
+// for (let index = 0; index < number.length; index++) {
+//     number[index] += 2
+//     console.log(number[index]);
+// }
+
+
+// var sum = 0;
+// for (let index = 0; index < number.length; index++) {
+//     sum += number[index]
+// }
+// console.log(sum);
+
+
+// even number print
+// for (let index = 0; index < number.length; index++) {
+//     if (number[index] % 2 === 0) {
+//         console.log(`${number[index]} is a Even number`);
+//     }
+// }
+
+
+// even number print and sum
+// var sum = 0
+// for (let index = 0; index < number.length; index++) {
+//     if (number[index] % 2 === 0) {
+//         console.log(`${number[index]} is a Even number`);
+//         sum += number[index]
+//     }
+// }
+// console.log(sum);
+
+
+
+
+// odd number print
+// for (let index = 0; index < number.length; index++) {
+//     const element = number[index];
+//     if (element % 2 === 1) {
+//         console.log(`${number[index]} is a Odd number`);
+//     }
+// }
+
+
+// odd number print or sumition
+// var sum = 0
+// for (let index = 0; index < number.length; index++) {
+//     const element = number[index];
+//     if (element % 2 === 1) {
+//         console.log(`${number[index]} is a Odd number`);
+//         sum += element
+//     }
+// }
+// console.log(sum);
