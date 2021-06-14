f6473162bdd506fc477d014cef8ca26f423f1a74
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-06-14 08:55:23 +0600

    20 JS Array Declaration addad

---
+++ array/20 JS Array Declaration.js
@@ -0,0 +1,45 @@
+var arr = []
+    // console.log(arr);
+
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    // console.log(arr);
+    // console.log(arr[0]);
+    // console.log(arr[8]);
+
+
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+arr[9] = 10;
+arr[20] = 21;
+// console.log(arr);
+// console.log(arr[9]);
+// console.log(arr[20]);
+
+// console.log(arr.length);
+
+
+// for (const number of arr) {
+//     console.log(number);
+// }
+
+
+
+var str = Array(1, 2, 3, 4, 5, 6, 7, 8, 9)
+console.log(str[0]);
+console.log(str[1]);
+console.log(str[2]);
+console.log(str[8]);
+
+
+
+var str2 = new Array
+str2[0] = 'Ehasan'
+str2[1] = 'Sanaullah'
+str2[2] = 'Tuhin'
+str2[3] = 'Sohidul'
+str2[4] = 'Nuru'
+
+console.log(str2[0]);
+console.log(str2[2]);
+console.log(str2[4]);
