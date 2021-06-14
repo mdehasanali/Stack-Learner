7fa9377f3a2f170ae3b7cbf50c4e495cd564febb
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-06-14 08:32:39 +0600

    string length mathor added

---
+++ 020 String length.js
@@ -0,0 +1,25 @@
+var str = 'My name is Md. Ehasan Ali'
+var length = 0
+
+for (;;) {
+    if (str.charAt(length) == '') {
+        break
+    } else {
+        length++
+    }
+}
+console.log(length);
+
+
+while (true) {
+    if (str.charAt(length) == '') {
+        break
+    } else {
+        length++
+    }
+}
+console.log(length);
+
+console.log(str.length);
+
+console.log('my name is ehasan i amfdsnkfksdfdkfksf'.length);
