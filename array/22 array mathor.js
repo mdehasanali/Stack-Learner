3bc0f5c5fb5f240af8666abc8aa3dc2075d08f02
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-06-14 12:02:40 +0600

    arrar mathor added

---
+++ array/22 array mathor.js
@@ -0,0 +1,34 @@
+var number = [1, 2, 3, 4, 5, 6, 7, 8]
+
+// copy korbe
+// number.copyWithin(3, 1, 4)
+// console.log(number);
+
+
+//laste akti number add korbe
+// number.push(9)
+// console.log(number);
+
+
+//laste akti number delete korbe
+// number.pop()
+// console.log(number);
+
+
+// first a akti number add korbe
+// number.unshift(0)
+// console.log(number);
+
+
+// first a akti number delete korbe
+// number.shift()
+// console.log(number);
+
+
+// number.splice(3, 0, 4, 5)
+// number.splice(8, 0, 9)
+// console.log(number);
+
+
+// number.splice(5, number.length)
+// console.log(number);
