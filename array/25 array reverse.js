214dc59272ac880ec3d4d9c497d33a2b0ca31c42
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-06-16 09:09:40 +0600

    array reverus

--- array/25 array reverse.js
+++ array/25 array reverse.js
@@ -9,4 +9,12 @@ var array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 
-console.log(array.reverse());
+// console.log(array.reverse());
+
+
+for (let index = 0; index < array.length / 2; index++) {
+    var temp = array[index]
+    array[index] = array[array.length - 1 - index]
+    array[array.length - 1 - index] = temp
+}
+console.log(array);
