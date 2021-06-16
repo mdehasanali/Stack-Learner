b1614ffacbd6d19da0bed87b32bead0db8b907bf
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-06-16 09:50:52 +0600

    array mathor added samething

--- array/22 array mathor.js
+++ array/22 array mathor.js
@@ -32,3 +32,51 @@ var number = [1, 2, 3, 4, 5, 6, 7, 8]
 
 // number.splice(5, number.length)
 // console.log(number);
+
+
+// fill methods a ja dibo tai sob index a show korbe
+// var array = [1, 5, 4, 9, 3, 7, 8, 6, 2]
+// console.log(array.fill(true));
+// console.log(array.fill(0));
+
+
+// array index er por ja show korabo tai show korbe
+// var array = [1, 5, 4, 9, 3, 7, 8, 6, 2]
+// console.log(array.join());
+// console.log(array.join(''));
+// console.log(array.join(', '));
+// console.log(array.join(' '));
+// console.log(array.join(' | '));
+
+
+// array1 theke array2 jog kora
+// var array1 = [1, 5, 4, 9, 3, 7, 8, 6, 2]
+// var array2 = [1, 5, 4, 9, 3, 7, 8, 6, 2]
+// console.log(array1.concat(array2));
+
+
+// array kina check korbe 
+var array = [1, 5, 4, 9, 3, 7, 8, 6, 2]
+var array2 = Array.isArray(array);
+console.log(array2);
+
+
+
+
+
+
+// var a = [1, 2]
+// var b = a
+// b[0] = 5
+// console.log(a[0]);
+
+
+
+//  from methods holo clone kora
+// var a = [1, 2]
+// var b = Array.from(a)
+
+// b[0] = 5
+
+// console.log(a);
+// console.log(b);
