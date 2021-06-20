9f5cab7769baa95d8467646db66256f32827975d
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-06-20 08:27:31 +0600

    Function colling

---
+++ Function/30 Defining a Function in Javascript.js
@@ -0,0 +1,33 @@
+// function er niyom
+
+
+function functionName() {
+    console.log('I am Function');
+}
+
+
+// add 
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+
+function add() {
+    var a = 10
+    var b = 20
+    console.log(a + b);
+}
+
+function sub() {
+    var a = 50
+    var b = 30
+    console.log(a - b);
+}
+
+functionName()
+
+// for (let index = 0; index < 10; index++) {
+//     functionName()
+// }
+
+add()
+sub()
