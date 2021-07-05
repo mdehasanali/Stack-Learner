c13cb8ca8543f8750028c63ca523176c094c2f23
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-07-05 10:39:31 +0600

    Higher Order Function in Javascript

--- Function/37 first class function in Javascript.js
+++ Function/37 first class function in Javascript.js
@@ -34,4 +34,24 @@ setTimeout(() => {
 
 // 5. We can Pass Function as an Arguments
 
+function add(a, b) {
+    return a + b
+}
+
+
+function manipulate(a, b, func) {
+    var c = a + b
+    var d = a - b
+
+    function multiply() {
+        var m = func(a, b)
+        return c * d * m;
+    }
+
+    return multiply
+}
+var multiply = manipulate(5, 10, add)
+console.log(multiply());
+
+
 // 6. We can return functions from Another Function
